ea1a93444b76b2e6844289942befec479f2f8c8a
Raiyan Rizwan <114321382+RaiyanRizwan@users.noreply.github.com>
2022-12-19 19:50:11 -0800

    Add files via upload

---
+++ Challenge_1/c1_code.py
@@ -0,0 +1,46 @@
+# build up lst as we parse
+
+def max_calories(path):
+	calorie_lst = []
+
+	with open(path, 'r') as f:
+		sm, mx = 0, 0
+		for line in f.readlines():
+			if line == '\n':
+				calorie_lst.append(sm)
+				mx, sm = sm if sm > mx else mx, 0
+			else:
+				sm += eval(line)
+
+	return mx
+
+	# f.read().split('\n\n') built-ins O(n) time anyways... we can get more efficient control on our own
+	# could also split into paragraphs & stop summing if > max, but likely not always more efficient (two O(n) processes)
+
+def top_three_calories(path):
+	max_lst = []
+
+	with open(path, 'r') as f:
+		sm = 0
+		for line in f.readlines():
+			if line == '\n':
+				i, sml = min_helper(max_lst) if len(max_lst) else (0, 0)
+				if len(max_lst) < 3 or (sm > sml and max_lst.pop(i)):
+					max_lst.append(sm)
+				sm = 0
+			else:
+				sm += eval(line)
+
+	return sum(max_lst)
+
+def min_helper(lst): #	A utility function that returns the minimum of a non-empty list and its index.
+	assert len(lst)
+	mn = 0
+	for i in range(1, len(lst)):
+		if lst[i] < lst[mn]:
+			mn = i
+	return (mn, lst[mn])
+
+
+
+
